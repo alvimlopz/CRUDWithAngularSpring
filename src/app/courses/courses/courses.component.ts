f9e1dee27b469fe8c2588ba2c7dd6e754e990365
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-19 10:59:34 -0300

    Lista de cursos: criandoum Spinner ( Carregando)

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 })
 export class CoursesComponent implements OnInit{
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
   //coursesService : CoursesService;
   displayedColumns = ['name', 'category'];
@@ -18,7 +18,7 @@ export class CoursesComponent implements OnInit{
   constructor(private coursesService : CoursesService){
     //this.courses = []; //inicializando a variavel
    // this.coursesService = new CoursesService(); //instanciando
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
   ngOnInit(): void {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../models/course';
 import { HttpClient } from '@angular/common/http';
-import { first, tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 import { Pipe } from '@angular/core';
 
 @Injectable({
@@ -15,6 +15,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
+      delay(5000),
       tap(courses => console.log(courses))
     );
   }
