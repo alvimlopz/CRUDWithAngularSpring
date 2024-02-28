8647d3e4b2179b6ebea71aa730103a19cb345112
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-28 10:43:07 -0300

    Adicionando o botão para adicionar um novo curso

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -15,7 +15,7 @@ export class CoursesComponent implements OnInit{
   courses$: Observable<Course[]>;
 
   //coursesService : CoursesService;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(private coursesService : CoursesService, public dialog: MatDialog){
     //this.courses = []; //inicializando a variavel
