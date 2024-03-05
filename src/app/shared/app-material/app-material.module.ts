61aec84e05a5be140cebe3e0dbc50726d117eee7
alvaro rodrigues <alvim.lopez@gmail.com>
2024-03-05 16:02:33 -0300

    Criando o formulario

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -8,7 +8,8 @@ import { MatDialogModule } from '@angular/material/dialog'
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+
 
 
 
